2ca0694395094f2f14483cebc3c7e7655ba4612c
Kirill Zhitko <kirillzhitko@gmail.com>
2024-01-19 20:32:02 +0300

    FEAT: оформление текста в теме

--- app/src/main/java/kanti/test/myhome/ui/screen/MainScreen.kt
+++ app/src/main/java/kanti/test/myhome/ui/screen/MainScreen.kt
@@ -12,17 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kanti.test.myhome.R
@@ -32,16 +28,6 @@ import kanti.test.myhome.R
 fun TopBar(
 	navController: NavController
 ) {
-	val circeFont = remember {
-		FontFamily(fonts = arrayOf(Font(R.font.circe)))
-	}
-	val tabText = @Composable { text: String ->
-		Text(
-			text = text,
-			fontFamily = circeFont,
-			fontSize = 21.sp
-		)
-	}
 
 	Column(
 		modifier = Modifier
@@ -49,11 +35,7 @@ fun TopBar(
 	) {
 		CenterAlignedTopAppBar(
 			title = {
-				Text(
-					text = stringResource(id = R.string.app_name),
-					fontFamily = circeFont,
-					fontSize = 21.sp
-				)
+				Text(text = stringResource(id = R.string.app_name))
 			}
 		)
 		var selectedTab by rememberSaveable {
@@ -72,9 +54,7 @@ fun TopBar(
 					selectedTab = 0
 					navController.navigate(route = camerasRoute)
 				},
-				text = {
-					tabText(stringResource(id = R.string.cameras))
-				},
+				text = { Text(text = stringResource(id = R.string.cameras)) },
 				interactionSource = MutableInteractionSource()
 			)
 			Tab(
@@ -83,9 +63,7 @@ fun TopBar(
 					selectedTab = 1
 					navController.navigate(route = doorsRoute)
 				},
-				text = {
-					tabText(stringResource(id = R.string.doors))
-				}
+				text = { Text(text = stringResource(id = R.string.doors)) }
 			)
 		}
 	}
--- app/src/main/java/kanti/test/myhome/ui/theme/Type.kt
+++ app/src/main/java/kanti/test/myhome/ui/theme/Type.kt
@@ -2,33 +2,25 @@ package kanti.test.myhome.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import kanti.test.myhome.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
 	bodyLarge = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.Normal,
-		fontSize = 16.sp,
-		lineHeight = 24.sp,
-		letterSpacing = 0.5.sp
-	)
-	/* Other default text styles to override
+		fontFamily = FontFamily(fonts = arrayOf(Font(R.font.circe))),
+		fontSize = 17.sp
+	),
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = FontFamily(fonts = arrayOf(Font(R.font.circe))),
+        fontSize = 21.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+	titleSmall = TextStyle(
+		fontFamily = FontFamily(fonts = arrayOf(Font(R.font.circe))),
+		fontSize = 17.sp
+	)
 )
